c0bfa3f9f11cad267a85b3539b04198cc63347b7
Jhônatan Santana <jhonatansantanati@gmail.com>
2025-04-19 12:33:51 -0300

    test: create test to Category Entity

--- domain/src/test/java/api/catalogo/domain/category/CategoryTest.java
+++ domain/src/test/java/api/catalogo/domain/category/CategoryTest.java
@@ -1,4 +1,28 @@
 package api.catalogo.domain.category;
 
+import com.api.catalogo.domain.category.Category;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CategoryTest {
+    @Test
+    public void givenAValidParams_whenCallNewCategory_thenInstantiateACategory() {
+        final var expectedName = "a Name";
+        final var expectedDescription =  "a some description";
+        final var expectedIsActive =  "a some description";
+
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        Assertions.assertNotNull(actualCategory);
+        Assertions.assertNotNull(actualCategory.getId());
+        Assertions.assertEquals(expectedName, actualCategory.getName());
+        Assertions.assertEquals(expectedDescription, actualCategory.getDescription());
+        Assertions.assertEquals(expectedIsActive, actualCategory.isActive());
+
+        Assertions.assertNotNull(actualCategory.getCreatedAt());
+        Assertions.assertNotNull(actualCategory.getUpdatedAt());
+        Assertions.assertNull(actualCategory.getDeletedAt());
+
+    }
 }
