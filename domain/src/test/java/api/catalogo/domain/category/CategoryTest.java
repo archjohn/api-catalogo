dbca7fafa957f9cc77136066a56f598ac0752baa
Jhônatan Santana <jhonatansantanati@gmail.com>
2025-04-20 13:44:23 -0300

    feat: create Interface Gateway for Category

--- domain/src/test/java/api/catalogo/domain/category/CategoryTest.java
+++ domain/src/test/java/api/catalogo/domain/category/CategoryTest.java
@@ -87,11 +87,11 @@ public class CategoryTest {
         final String expectedName = """
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. \n
             Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-            when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
-            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. 
-            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
+            when an unknown printer took a galley of type and scrambled it to make a type specimen book.\s
+            It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.\s
+            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,\s
             and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-            """;
+           \s""";
         final var expectedErrorMessage = "'name' must be between 3 and 255 characters";
         final var expectedErrorCount = 1;
         final var expectedDescription = "a some description";
